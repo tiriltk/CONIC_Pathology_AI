c89db707dca31268bef0c94514010ab422a11179
Tiril Kopland <tirilkop@gmail.com>
2025-08-12 22:07:30 +0200

    Updated env_setup.sh and environment.yml for my setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,9 @@
 conda env create -f environment.yml
 conda activate aughovernet
-conda install pytorch==2.5.1 torchvision==0.20.1 cudatoolkit==11.8 -c pytorch
-pip install albumentations>=2.0.4 --no-binary imgaug,albumentations
+conda install pytorch==2.5.1 torchvision==0.20.1 -c pytorch
+#python -m pip install --upgrade pip
+#pip install torch==2.5.1 torchvision==0.20.1
+pip install "albumentations>=2.0.4" --no-binary imgaug,albumentations
 pip install opencv-python
 pip install pretrainedmodels
 pip install efficientnet_pytorch
